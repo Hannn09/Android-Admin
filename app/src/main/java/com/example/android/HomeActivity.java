9ce30a54f5090256eb45fcad37811a4009fca0b4
Reihan Almas Hediawan <reihanalmas@gmail.com>
2020-09-18 18:25:59 +0700

    Nav

--- app/src/main/java/com/example/android/HomeActivity.java
+++ app/src/main/java/com/example/android/HomeActivity.java
@@ -1,24 +1,42 @@
 package com.example.android;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
-
     private ArrayList<card_model> modelArrayList;
     private adapter adapter;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         getSupportActionBar().hide();
 
@@ -43,5 +61,15 @@ public class HomeActivity extends AppCompatActivity {
         viewPager.setPadding(5,0,5,0);
     }
 
+    @Override
+    public void onBackPressed() {
 
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
 }
